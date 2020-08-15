f04c1135978ef513727a00cb08b7512597ba7000
Kier Davis <me@kierdavis.com>
2020-08-15 19:05:15 +0100

    This is already done

--- app/src/main/java/com/kierdavis/fuchsia/ui/ItemCardClickListener.kt
+++ app/src/main/java/com/kierdavis/fuchsia/ui/ItemCardClickListener.kt
@@ -3,6 +3,5 @@ package com.kierdavis.fuchsia.ui
 import com.kierdavis.fuchsia.model.ItemWithPictures
 
 interface ItemCardClickListener {
-    // TODO rename
     fun onItemCardClicked(itemWithPictures: ItemWithPictures)
 }
