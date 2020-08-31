6e2c7fd4fffcc9d5ab01618c7efab16b1f607550
Kier Davis <me@kierdavis.com>
2020-08-31 02:51:45 +0100

    Add button to create collections

--- app/src/main/java/com/kierdavis/fuchsia/ui/component/AllCollectionsComponent.kt
+++ app/src/main/java/com/kierdavis/fuchsia/ui/component/AllCollectionsComponent.kt
@@ -1,19 +1,56 @@
 package com.kierdavis.fuchsia.ui.component
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.kierdavis.fuchsia.R
+import kotlin.math.roundToInt
 
-class AllCollectionsComponent(context: Context, lifecycleOwner: LifecycleOwner) : Component(context, lifecycleOwner) {
-    // Data
-    val liveCollections = database.collectionDao().all()
+class AllCollectionsComponent(context: Context, lifecycleOwner: LifecycleOwner) :
+    Component(context, lifecycleOwner) {
 
     // Properties
     var onCollectionCardClickedListener
         get() = collectionsComponent.onCardClickedListener
-        set(it) { collectionsComponent.onCardClickedListener = it }
+        set(it) {
+            collectionsComponent.onCardClickedListener = it
+        }
+    var onAddButtonClickedListener: OnAddButtonClickedListener? = null
 
-    private val collectionsComponent = CollectionCardsComponent(context, lifecycleOwner, liveCollections)
+    private val collectionsComponent =
+        CollectionCardsComponent(context, lifecycleOwner, database.collectionDao().all())
+    private val addButton = FloatingActionButton(context).apply {
+        setImageResource(android.R.drawable.ic_input_add)
+        ImageViewCompat.setImageTintList(
+            this,
+            ColorStateList.valueOf(ContextCompat.getColor(context, R.color.secondaryTextColor))
+        )
+        size = FloatingActionButton.SIZE_AUTO
+        isClickable = true
+        isFocusable = true
+        setOnClickListener { onAddButtonClickedListener?.onAddCollectionButtonClicked() }
+    }
+    private val layout = FrameLayout(context).apply {
+        addView(collectionsComponent.view, FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT))
+        addView(addButton, FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+            gravity = Gravity.END or Gravity.BOTTOM
+            val margin = context.resources.getDimension(R.dimen.fab_margin).roundToInt()
+            setMargins(0, 0, margin, margin)
+        })
+    }
     override val view: View
-        get() = collectionsComponent.view
+        get() = layout
+
+
+    interface OnAddButtonClickedListener {
+        fun onAddCollectionButtonClicked()
+    }
 }
--- app/src/main/java/com/kierdavis/fuchsia/ui/fragment/CollectionsFragment.kt
+++ app/src/main/java/com/kierdavis/fuchsia/ui/fragment/CollectionsFragment.kt
@@ -1,24 +1,56 @@
 package com.kierdavis.fuchsia.ui.fragment
 
+import android.content.Context
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.*
 import androidx.navigation.fragment.findNavController
+import com.kierdavis.fuchsia.database.AppDatabase
+import com.kierdavis.fuchsia.model.Collection
 import com.kierdavis.fuchsia.ui.component.AllCollectionsComponent
 import com.kierdavis.fuchsia.ui.component.CollectionCardComponent
+import kotlinx.coroutines.launch
 
-class CollectionsFragment : ComponentFragment<AllCollectionsComponent>(), CollectionCardComponent.OnClickedListener {
+class CollectionsFragment : ComponentFragment<AllCollectionsComponent>(),
+    CollectionCardComponent.OnClickedListener, AllCollectionsComponent.OnAddButtonClickedListener {
+
+    private val viewModel by viewModels<Model> {
+        Model.Factory(requireContext())
+    }
+
     override fun onCreateComponent(): AllCollectionsComponent =
         AllCollectionsComponent(requireContext(), viewLifecycleOwner).apply {
             onCollectionCardClickedListener = this@CollectionsFragment
+            onAddButtonClickedListener = this@CollectionsFragment
         }
-
-    /*
-    private fun onNewCollectionClicked() {
-        viewModel.createCollection().observe(this) { collection ->
-            findNavController().navigate(CollectionsFragmentDirections.actionCollectionsToEditCollection(collection.id))
-        }
-    }
-     */
 
     override fun onCollectionCardClicked(id: Long) {
         findNavController().navigate(CollectionsFragmentDirections.actionCollectionsToEditCollection(id))
+    }
+
+    override fun onAddCollectionButtonClicked() {
+        viewModel.addCollection().observe(this) { collectionId ->
+            findNavController().navigate(CollectionsFragmentDirections.actionCollectionsToEditCollection(collectionId))
+        }
+    }
+
+
+    private class Model(context: Context) : ViewModel() {
+        private val database = AppDatabase.getInstance(context)
+
+        fun addCollection(): LiveData<Long> {
+            val liveCollectionId = MutableLiveData<Long>()
+            viewModelScope.launch {
+                liveCollectionId.postValue(database.collectionDao().insert(Collection()))
+            }
+            return liveCollectionId
+        }
+
+
+        class Factory(private val context: Context) : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return Model(context) as T
+            }
+        }
     }
 }
